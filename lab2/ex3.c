08709d219dd762d3a88ef1b34c801be34e55447e
ArtemK2007 <artemkryachko2007@gmail.com>
2025-03-30 22:07:38 +0200

    Update ex3.c

--- lab2/ex3.c
+++ lab2/ex3.c
@@ -1,32 +1,45 @@
 #include <stdio.h>
-void smoothArray(int arr[], int n) {
-    int temp[n];
-    temp[0] = (arr[0] + arr[1]) / 2;
-    temp[n - 1] = (arr[n - 2] + arr[n - 1]) / 2;
-    for (int i = 1; i < n - 1; i += 1) {
-        temp[i] = (arr[i - 1] + arr[i] + arr[i + 1]) / 3;
-    }
-    for (int i = 0; i < n; i += 1) {
-        arr[i] = temp[i];
-    }
-}
+#include <stdlib.h>
+#include <math.h>
 int main() {
-    system("chcp 65001");
+system("chcp 65001");
     int n;
-    printf("Введіть кількість елементів: ");
+    double a;
+    printf("Введіть розмір масивів n: ");
     scanf("%d", &n);
-    int arr[n];
-    printf("Введіть %d елементи: ", n);
+    printf("Введіть число A: ");
+    scanf("%lf", &a);
+    double x[n];
+    double y[n];
+    printf("Введіть елементи масиву X:\n");
     for (int i = 0; i < n; i += 1) {
-        scanf("%d", &arr[i]);
+        printf("x[%d] = ", i);
+        scanf("%lf", &x[i]);
     }
-    smoothArray(arr, n);
-    printf("Оновлений масив: ");
+    printf("Введіть елементи масиву Y:\n");
     for (int i = 0; i < n; i += 1) {
-        printf("%d ", arr[i]);
+        printf("y[%d] = ", i);
+        scanf("%lf", &y[i]);
     }
-    printf("\n");
+    double z[n];
+    int z_count = 0;
+    printf("\nЕлементи, для яких |x_i - y_i| < %.2lf:\n", a);
+
+    for (int i = 0; i < n; i += 1) {
+        if (fabs(x[i] - y[i]) < a) {
+            printf("Елемент з номером %d: |%.2lf - %.2lf| = %.2lf\n", i + 1, x[i], y[i], fabs(x[i] - y[i]));
+            z[z_count] = x[i] - y[i];
+            z_count++;
+        }
+    }
+    if (z_count > 0) {
+        printf("\nМасив Z (різниці елементів): ");
+        for (int i = 0; i < z_count; i += 1) {
+            printf("%.2lf ", z[i]);
+        }
+        printf("\n");
+    } 
     getchar();
     getchar();
-    return 0;
+    return 0; 
 }
